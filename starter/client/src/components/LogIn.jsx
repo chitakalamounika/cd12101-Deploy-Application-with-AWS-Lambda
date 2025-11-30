fdeaa91309117fe59fdaaf1ef95fe4af6a622ee1
chitakalamounika <93363612+chitakalamounika@users.noreply.github.com>
2025-11-30 15:13:18 +0000

    updated version

--- starter/client/src/components/LogIn.jsx
+++ starter/client/src/components/LogIn.jsx
@@ -1,16 +1,15 @@
-import { useAuth0 } from '@auth0/auth0-react'
-import React from 'react'
-import { Button } from 'semantic-ui-react'
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from 'semantic-ui-react';
 
-export const LogIn = () => {
-  const { loginWithRedirect } = useAuth0()
+export default function LogIn() {
+  const { loginWithRedirect } = useAuth0();
   return (
-    <div>
+    <div style={{ textAlign: 'center', marginTop: '5em' }}>
       <h1>Please log in</h1>
-
       <Button onClick={() => loginWithRedirect()} size="huge" color="olive">
-        Log in
+        Log In
       </Button>
     </div>
-  )
+  );
 }
