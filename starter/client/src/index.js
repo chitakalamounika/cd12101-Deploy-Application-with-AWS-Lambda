a9293340a3f95527834405e738a16a515ee74701
chitakalamounika <93363612+chitakalamounika@users.noreply.github.com>
2025-11-30 14:50:57 +0000

    updated index

--- starter/client/src/index.js
+++ starter/client/src/index.js
@@ -1,21 +1,29 @@
-import { Auth0Provider } from '@auth0/auth0-react'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'semantic-ui-css/semantic.min.css'
-import App from './App'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+import App from './App';
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
-ReactDOM.render(
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE; // https://todo-api
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    redirectUri={window.location.origin}
-    audience={`https://${domain}/api/v2/`}
-    scope="read:todo write:todo delete:todo"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience,
+      scope: 'openid profile email read:todo write:todo delete:todo',
+    }}
+    cacheLocation="localstorage"
+    useRefreshTokens={true}
   >
     <App />
-  </Auth0Provider>,
-  document.getElementById('root')
-)
+  </Auth0Provider>
+);
