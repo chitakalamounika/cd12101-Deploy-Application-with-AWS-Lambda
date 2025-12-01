369fbf634f7f0f84a3ea1ae21f856f00f5d9a71a
chitakalamounika <93363612+chitakalamounika@users.noreply.github.com>
2025-12-01 02:54:20 +0000

    deleted token.js

--- starter/client/src/token.js
+++
@@ -1,9 +0,0 @@
-import { useAuth0 } from '@auth0/auth0-react';
-
-export function useApiToken() {
-  const { getAccessTokenSilently } = useAuth0();
-  return async () =>
-    getAccessTokenSilently({
-      audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-    });
-}
